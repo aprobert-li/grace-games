1bb500853614b25a275e8a49e06d10f83fa28e8c
aprobert-li <aprobert@gmail.com>
2020-03-25 10:17:03 -0700

    Got random number and audio working

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
+var randomNumber;
+var usedArray = [];
 
-var randomNumber = Math.floor((Math.random()*100));
-var randTest = Math.floor((Math.random()*10));
-var showNumber = document.querySelector('#randomNumber');
-showNumber.innerHTML = randTest;
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", getNumber);
+
+function getNumber() {
+  console.log("NEW CLICK");
+  randomNumber = Math.floor(Math.random() * 10);
+  if (!usedArray.includes(randomNumber)) {
+    console.log(randomNumber);
+    usedArray.push(randomNumber);
+    console.log(usedArray);
+    return randomNumber;
+  } else {
+    console.log("already used");
+    if (usedArray.length < 10) {
+      getNumber();
+    } else {
+      btn.removeEventListener("click", getNumber);
+    }
+  }
+}
+
 var audio;
 var audioBoom;
 
@@ -10,15 +30,17 @@ var numbers = document.querySelectorAll(".numbers");
 for (n of numbers) {
   n.addEventListener("click", function() {
     var chosen = this.dataset.number;
+    var chosenEl = document.getElementById(this.id);
     console.log(chosen);
 
-    if (randTest !== chosen) {
+    if (randomNumber !== chosen) {
         audio = document.createElement('audio');
         audio.src=`audio/${chosen}.mp3`;
         audio.play();
     }
     
-    if (randTest == chosen) {
+    if (randomNumber == chosen) {
+        chosenEl.style.backgroundColor = "yellow";
         audio.pause();
         audioBoom = document.createElement('audio');
         audioBoom.src="audio/correct.mp3";
