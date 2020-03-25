96f54f444b2f1e9f11223c9c1faabdbe407a8454
aprobert-li <aprobert@gmail.com>
2020-03-25 15:07:46 -0700

    Added audio and other interaction controls

--- script.js
+++ script.js
@@ -13,7 +13,9 @@ btn.addEventListener("click", getNumber);
 var cover = document.querySelector('#cover');
 
 function getNumber() {
+  btn.removeEventListener("click", getNumber);
   cover.style.display = "none";
+  replay.style.display = "inline-block";
   console.log("NEW CLICK");
   randomNumber = Math.floor(Math.random() * 10);
   if (!usedArray.includes(randomNumber)) {
@@ -29,15 +31,17 @@ function getNumber() {
     console.log("already used");
     if (usedArray.length < 10) {
       getNumber();
-    } else {
+    } 
+    else {
       btn.removeEventListener("click", getNumber);
-      alert("COMPLETE!");
     }
   }
+  
 }
 
 
 function choosing() {
+  var counter = 0;
   var numbers = document.querySelectorAll(".numbers");
   for (n of numbers) {
     n.addEventListener("click", function() {
@@ -58,7 +62,21 @@ function choosing() {
           audioBoom.src="audio/correct.mp3";
           audioBoom.play();
           cover.style.display = "block";
+          btn.addEventListener("click", getNumber);
+          replay.style.display = "none";
+          counter++;
+          if(counter == 10) {
+            alert("ALL DONE");
+          }
       }
     });
   }
 }
+
+var replay = document.querySelector('#replay');
+replay.addEventListener("click", function() {
+  numberAudio.play();
+})
+
+
+
