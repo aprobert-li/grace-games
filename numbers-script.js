fb6f2075fd353e23f6a68c46502b17172e4a7ad6
aprobert-li <aprobert@gmail.com>
2020-03-26 10:23:42 -0700

    Added files for letters and code for mobile first

---
+++ numbers-script.js
@@ -0,0 +1,85 @@
+var randomNumber;
+var usedArray = [];
+//var numberCount = 0;
+
+var audio;
+var numberAudio;
+var audioBoom;
+
+var btn = document.querySelector("button");
+
+btn.addEventListener("click", getNumber);
+btn.addEventListener("touchstart", getNumber);
+
+var cover = document.querySelector('#cover');
+
+function getNumber() {
+  btn.removeEventListener("click", getNumber);
+  btn.removeEventListener("touchstart", getNumber);
+  cover.style.display = "none";
+  replay.style.display = "inline-block";
+  console.log("NEW CLICK");
+  randomNumber = Math.floor(Math.random() * 10);
+  if (!usedArray.includes(randomNumber)) {
+    console.log(randomNumber);
+    usedArray.push(randomNumber);
+    console.log(usedArray);
+    numberAudio = document.createElement('audio');
+    numberAudio.src=`audio/${randomNumber}.mp3`;
+    numberAudio.play();
+    choosing();
+    return randomNumber;
+  } else {
+    console.log("already used");
+    if (usedArray.length < 10) {
+      getNumber();
+    } 
+    else {
+      btn.removeEventListener("click", getNumber);
+    }
+  }
+  
+}
+
+
+function choosing() {
+  var counter = 0;
+  var numbers = document.querySelectorAll(".numbers");
+  function chooseNumber() {
+    var chosen = this.dataset.number;
+    var chosenEl = document.getElementById(this.id);
+    console.log(chosen);
+
+    if (randomNumber !== chosen) {
+        audio = document.createElement('audio');
+        audio.src=`audio/${chosen}.mp3`;
+        audio.play();
+    }
+    
+    if (randomNumber == chosen) {
+        chosenEl.style.backgroundColor = "yellow";
+        audio.pause();
+        audioBoom = document.createElement('audio');
+        audioBoom.src="audio/correct.mp3";
+        audioBoom.play();
+        cover.style.display = "block";
+        btn.addEventListener("click", getNumber);
+        replay.style.display = "none";
+        counter++;
+        if(counter == 10) {
+          alert("ALL DONE");
+        }
+    }
+  }
+  for (n of numbers) {
+    n.addEventListener("click", chooseNumber);
+  }
+}
+
+var replay = document.querySelector('#replay');
+replay.addEventListener("click", function() {
+  numberAudio.play();
+})
+
+
+
--- script.js
+++ script.js
@@ -1,82 +1,10 @@
-var randomNumber;
-var usedArray = [];
-//var numberCount = 0;
+var numbersBtn = document.querySelector('#playNumbers');
 
-var audio;
-var numberAudio;
-var audioBoom;
+numbersBtn.addEventListener("touchstart", goToPage);
 
-var btn = document.querySelector("button");
-
-btn.addEventListener("click", getNumber);
-
-var cover = document.querySelector('#cover');
-
-function getNumber() {
-  btn.removeEventListener("click", getNumber);
-  cover.style.display = "none";
-  replay.style.display = "inline-block";
-  console.log("NEW CLICK");
-  randomNumber = Math.floor(Math.random() * 10);
-  if (!usedArray.includes(randomNumber)) {
-    console.log(randomNumber);
-    usedArray.push(randomNumber);
-    console.log(usedArray);
-    numberAudio = document.createElement('audio');
-    numberAudio.src=`audio/${randomNumber}.mp3`;
-    numberAudio.play();
-    choosing();
-    return randomNumber;
-  } else {
-    console.log("already used");
-    if (usedArray.length < 10) {
-      getNumber();
-    } 
-    else {
-      btn.removeEventListener("click", getNumber);
-    }
-  }
-  
+function goToPage(event) {
+    numbersBtn.style.backgroundColor = "red";
+    window.location.href = "numbers.html";
+    event.preventDefault();
+    return false;
 }
-
-
-function choosing() {
-  var counter = 0;
-  var numbers = document.querySelectorAll(".numbers");
-  for (n of numbers) {
-    n.addEventListener("click", function() {
-      var chosen = this.dataset.number;
-      var chosenEl = document.getElementById(this.id);
-      console.log(chosen);
-
-      if (randomNumber !== chosen) {
-          audio = document.createElement('audio');
-          audio.src=`audio/${chosen}.mp3`;
-          audio.play();
-      }
-      
-      if (randomNumber == chosen) {
-          chosenEl.style.backgroundColor = "yellow";
-          audio.pause();
-          audioBoom = document.createElement('audio');
-          audioBoom.src="audio/correct.mp3";
-          audioBoom.play();
-          cover.style.display = "block";
-          btn.addEventListener("click", getNumber);
-          replay.style.display = "none";
-          counter++;
-          if(counter == 10) {
-            alert("ALL DONE");
-          }
-      }
-    });
-  }
-}
-
-var replay = document.querySelector('#replay');
-replay.addEventListener("click", function() {
-  numberAudio.play();
-})
-
-
-
